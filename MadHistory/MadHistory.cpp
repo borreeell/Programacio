d8c6569aab95b41039556a604134dc5392a25d72
borreeell <pborrell.lsgi@gmail.com>
2025-10-10 12:48:26 +0200

    Commit 10/10/2025

--- MadHistory/MadHistory.cpp
+++ MadHistory/MadHistory.cpp
@@ -1,45 +1,161 @@
-// MadHistory.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Pau Borrell
 
 #include <iostream>
+#include <string>
+
+#include <Windows.h>
 
 using namespace std;
+using namespace std::string_literals;
 
-int askNumber(int high, int low = 2);
+// Function headers
+string askText(string prompt);
+int askNumber(string prompt);
+float askFloat(string prompt);
+bool askBoolean(string prompt);
+void tellStory();
 
-//bool askBoolean()
-//{
-//
-//}
-//
-//string askText()
-//{
-//	
-//}
-
-
-
-void tellhistory()
+int main() 
 {
-	
+	SetConsoleOutputCP(CP_UTF8);
+
+	bool keepPlaying = true;
+
+	while (keepPlaying) {
+		tellStory();
+		cout << "\nDo you want to create a new adventure? (1 = yes, 0 = no): ";
+		cin >> keepPlaying;
+		cin.ignore();
+		cout << "\n";
+	}
+
+	cout << "Thanks for playing! Goodbye!\n";
+	return 0;
 }
 
-int main()
+// Function to take text from the user
+string askText(string prompt) 
 {
-	int number = askNumber(5);
-	cout << "You entered number: " << number << endl;
-
-	number = askNumber(10, 5);
+	string input;
+	cout << prompt;
+	getline(cin, input);
+	return input;
 }
 
-int askNumber(int high, int low)
+// Fuunction to take numbers from the users
+int askNumber(string prompt) 
 {
-	int num;
+	int input;
+	cout << prompt;
+	cin >> input;
+	cin.ignore();
+	return input;
+}
 
-	do {
-		cout << "Please enter a number" << " (" << low <<" - " << high << "): ";
-		cin >> num;
-	} while (num > high || num < low);
+// Function to take floats from the user
+float askFloat(string prompt) 
+{
+	float input;
+	cout << prompt;
+	cin >> input;
+	cin.ignore();
+	return input;
+}
 
-	return num;
+// Function to take booleans from the user
+bool askBoolean(string prompt) 
+{
+	int choice;
+	cout << prompt << " (1 = yes, 0 = no): ";
+	cin >> choice;
+	cin.ignore();
+	return choice == 1;
+}
+
+// Function to take chars from the user
+char askChar(string prompt) 
+{
+	char input;
+	cout << prompt;
+	cin >> input;
+	cin.ignore();
+	return input;
+}
+
+// Main story function
+void tellStory() {
+	// String variables
+	string characterName = askText("Enter character's name: ");
+	string object1 = askText("Enter an object grabbed: ");
+	string buildingType = askText("Enter building type: ");
+	string place = askText("Enter a nearby place: ");
+	string object2 = askText("Enter an object adjusted: ");
+	string animal = askText("Enter an animal: ");
+	string object3 = askText("Enter old object fixed: ");
+
+	// Char variable
+	char timeOfDay = askChar("Enter first letter of time of day (M/A/E): ");
+	char moodChar = askChar("Enter one letter representing mood (H = happy, S = sad): ");
+
+	// Int variables
+	int numHoursFixing = askNumber("Enter hours spent fixing the object: ");
+	int numBeeps = askNumber("Enter number of beeps: ");
+	int characterAge = askNumber("Enter character age: ");
+	int numFriends = askNumber("Enter number of friends to meet: ");
+
+	// Float variable
+	float beepDuration = askFloat("Enter duration of one beep in seconds: ");
+
+	// Boolean variables
+	bool isSunny = askBoolean("Is the weather sunny?");
+	bool isMechanicalBird = askBoolean("Is the bird mechanical?");
+	bool isHappyEnding = askBoolean("Is it a happy ending?");
+
+	// Math operation
+	float totalBeepTime = numBeeps * beepDuration;
+
+	// Story output
+	cout << "\n--- Your Adventure Story ---\n";
+	cout << "Early one ";
+	if (timeOfDay == 'M') cout << "morning, ";
+	else if (timeOfDay == 'A') cout << "afternoon, ";
+	else cout << "evening, ";
+	cout << characterName << " grabbed " << object1
+		<< " and stepped outside " << buildingType
+		<< " near " << place << ".\n";
+
+	cout << "The sky was " << (isSunny ? "clear and bright" : "cloudy")
+		<< " - a perfect day for a walk. " << characterName
+		<< " adjusted " << object2
+		<< " and smiled when seeing a " << (isMechanicalBird ? "mechanical " : "")
+		<< animal << " flying across the water.\n";
+
+	cout << characterName << " had spent " << numHoursFixing << " hours fixing an old "
+		<< object3 << " the night before and felt proud it finally worked.\n";
+
+	if (totalBeepTime > 10.0f) {
+		cout << "The beeping from the watch lasted quite long: "
+			<< totalBeepTime << " seconds!\n";
+	}
+
+	cout << "While passing by, " << characterName
+		<< " pressed a small button on the wristwatch. "
+		<< "A cheerful sound beeped " << numBeeps << " times.\n";
+
+	if (characterAge < 18 && numFriends > 2) {
+		cout << characterName << " looked forward to playing with all friends.\n";
+	}
+	else if (characterAge >= 18 && numFriends > 0) {
+		cout << characterName << " was excited to meet friends later at the park.\n";
+	}
+
+	cout << characterName << " is " << characterAge
+		<< " years old, and a favorite hobby is spending time with friends. "
+		<< "Later that day, they planned to meet " << numFriends << " friends at the park.\n";
+
+	cout << "Mood today is '" << moodChar << "'. "
+		<< "The weather was " << (isSunny ? "sunny" : "not sunny")
+		<< ", the " << animal << " was " << (isMechanicalBird ? "mechanical" : "real")
+		<< ", and " << characterName << " felt "
+		<< (isHappyEnding ? "truly content with a peaceful life." : "a bit uncertain about the day.") << "\n";
 }
