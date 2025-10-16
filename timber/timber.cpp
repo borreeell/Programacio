8fdc2a39178dc2d04a79a23f5abc886d0abf3ee0
borreeell <pborrell.lsgi@gmail.com>
2025-10-16 11:16:01 +0200

    Commit 3 16/10/2025
    Replaces the SFML demo code with initial Timber game setup, including window creation, background, tree, player, and bee textures. Adds an NPC struct for future use and organizes code into sections for visuals, event handling, and drawing. Prepares the main loop for further game logic implementation.

--- timber/timber.cpp
+++ timber/timber.cpp
@@ -1,21 +1,84 @@
+// timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Incloure llibreries
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
+
+// Espais de noms
+using namespace std;
+using namespace sf;
+
+struct NPC {
+    Sprite sprite;
+    bool active;
+    float speed;
+    int maxHeight;
+    int maxSpeed;
+    int sentit; // 1 = dreta, -1 esquerra
+    float posicioInicialX;
+
+    NPC(Texture& texture, int maxHeight_, int maxSpeed_, int sentit_, float posicioInicialX_)
+        :sprite(texture), active(false), speed(0),
+        maxHeight(maxHeight_), maxSpeed(maxSpeed_),
+        sentit(sentit_), posicioInicialX(posicioInicialX_) {
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // -----------------------------
+    // Creacio i elements visuals
+    // -----------------------------
+    VideoMode vm({ 1920, 1080 });
+    RenderWindow window(vm, "Timber!!!", State::Windowed);
+    
+    // Textura del fons
+    Texture textureBackground("graphics/background.png");
+    Sprite spriteBackground(textureBackground);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
+    // Textura de l'arbre 1
+    Texture textureTree1("graphics/tree.png");
+    Sprite spriteTree1(textureTree1);
+    spriteTree1.setPosition({ 810, 0 });
+
+    // Textura del jugador
+    Texture texturePlayer("graphics/player.png");
+    Sprite spritePlayer(texturePlayer);
+    spritePlayer.setPosition({ 580, 720 });
+
+    // Textura de l'abella
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+
+
+
+    // -----------------------------
+    // Bucle principal del joc
+    // -----------------------------
+    while (window.isOpen()) {
+        // Gestio d'events
+        while (const optional event = window.pollEvent()) {
+            if (event->is<Event::Closed>()) {
                 window.close();
+            }
         }
 
+        // -----------------------------
+        // Actualitzacio del joc
+        // -----------------------------
+
+        // -----------------------------
+        // Dibuix
+        // -----------------------------
         window.clear();
-        window.draw(shape);
+        window.draw(spriteBackground);
+        window.draw(spriteTree1);
+        window.draw(spritePlayer);
+
         window.display();
     }
+
+    return 0;
 }
