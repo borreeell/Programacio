89e5777262e0ed11131d0dca8fe8b58305ca42a8
borreeell <pborrell.lsgi@gmail.com>
2025-10-17 12:53:14 +0200

    Commit 17/10/2025: Continuar timber

--- timber/timber.cpp
+++ timber/timber.cpp
@@ -16,6 +16,12 @@ using namespace sf;
 // Enumeracions i constants globals
 // -------------------------------------
 const int NUM_CLOUDS = 3;
+const int NUM_BRANCHES = 6;
+
+const float timeBarStartWidth = 400;
+const float timeBarHeight = 80;
+
+enum class Side { LEFT, RIGHT, NONE };
 
 // -------------------------------------
 // Estructura NPC (Abella/Nuvols)
@@ -40,6 +46,7 @@ struct NPC {
 // Prototips de funcions
 // -------------------------------------
 void updateNPC(NPC&, float);
+void updateBranchSprites(Side[], Sprite[]);
 
 int main()
 {
@@ -47,7 +54,7 @@ int main()
     // Creacio i elements visuals
     // -------------------------------------
     VideoMode vm({ 1920, 1080 });
-    RenderWindow window(vm, "Timber!!!", State::Windowed);
+    RenderWindow window(vm, "Timber", State::Windowed);
     
     // Textura del fons
     Texture textureBackground("graphics/background.png");
@@ -71,15 +78,62 @@ int main()
 
     NPC bee(textureBee, 500, 400, -1, 2000);
     NPC clouds[NUM_CLOUDS] = {
-        NPC(textureCloud, 0, 200, 1, -200),
+        NPC(textureCloud, 100, 200, 1, -200),
         NPC(textureCloud, 250, 200, 1, -200),
         NPC(textureCloud, 500, 200, 1, -200)
     };
 
     // -------------------------------------
+    // Textos i marcador
+    // -------------------------------------
+    Font font("fonts/KOMIKAP_.ttf");
+    Text messageText(font);
+    FloatRect textRect = messageText.getLocalBounds();
+    Text scoreText(font);
+    
+    messageText.setString("Press Enter to start!");
+    messageText.setCharacterSize(75);
+    messageText.setFillColor(Color::White);
+    messageText.setOrigin({ textRect.position.x + textRect.size.x / 2.0f, textRect.position.y + textRect.size.y / 2.0f });
+    messageText.setPosition({ 1920 / 2, 1080 / 2 });
+
+    scoreText.setCharacterSize(75);
+    scoreText.setString("Score = 0");
+    scoreText.setFillColor(Color::White);
+    scoreText.setPosition({ 20, 20 });
+
+    // -------------------------------------
     // Variables de joc
     // -------------------------------------
     Clock clock;
+
+    bool paused = true;
+    int score = 0;
+    float timeRemaining = 6.0f;
+    float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
+    RectangleShape timeBar(Vector2f(timeBarStartWidth, timeBarHeight));
+
+    timeBar.setFillColor(Color::Red);
+    timeBar.setPosition({ 1920 / 2 - timeBarStartWidth / 2, 980 });
+
+    // -------------------------------------
+    // Configuracio branques
+    // -------------------------------------
+    Texture textureBranch("graphics/branch.png");
+    Side branchPositions[NUM_BRANCHES];
+    Sprite branches[NUM_BRANCHES] = {
+        Sprite(textureBranch),Sprite(textureBranch),Sprite(textureBranch),
+        Sprite(textureBranch),Sprite(textureBranch),Sprite(textureBranch)
+    };
+
+    for (int i = 0; i < NUM_BRANCHES; i++) {
+        branches[i].setTexture(textureBranch);
+        branches[i].setPosition({ -2000, -2000 });
+
+        branches[i].setOrigin({ 220, 20 });
+        branchPositions[i] = Side::LEFT;
+    }
 
     // -------------------------------------
     // Bucle principal del joc
@@ -89,20 +143,45 @@ int main()
         while (const optional event = window.pollEvent()) {
             if (event->is<Event::Closed>()) window.close();
             if (const auto* key = event->getIf<Event::KeyPressed>()) {
-                if (key->scancode == Keyboard::Scancode::Escape) window.close();
+                if (key->scancode == Keyboard::Scancode::Escape) window.close(); // Utilitza la tecla ESC per sortir del joc
+
+                // Reiniciar el joc
+                if (key->scancode == Keyboard::Scancode::Enter) {
+                    paused = false;
+                    score = 0;
+                    timeRemaining = 6;
+                }
             }
         }
 
         // -------------------------------------
         // Actualitzacio del joc
         // -------------------------------------
-        Time dt = clock.restart();
+        if (!paused) {
+            Time dt = clock.restart();
 
-        // Entitats mobils
-        updateNPC(clouds[0], dt.asSeconds());
-        updateNPC(clouds[1], dt.asSeconds());
-        updateNPC(clouds[2], dt.asSeconds());
-        updateNPC(bee, dt.asSeconds());
+            timeRemaining -= dt.asSeconds();
+            timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, 800));
+
+            if (timeRemaining <= 0) {
+                paused = true;
+                messageText.setString("Out of time!");
+                textRect = messageText.getLocalBounds();
+                messageText.setOrigin({ (textRect.position.x + (textRect.size.x / 2.0f)), (textRect.position.y + (textRect.size.y / 2.0f)) });
+            }
+
+            // Entitats mobils
+            updateNPC(clouds[0], dt.asSeconds());
+            updateNPC(clouds[1], dt.asSeconds());
+            updateNPC(clouds[2], dt.asSeconds());
+            updateNPC(bee, dt.asSeconds());
+
+            updateBranchSprites(branchPositions, branches);
+
+            stringstream ss;
+            ss << "Score = " << score;
+            scoreText.setString(ss.str());
+        }
 
         // -------------------------------------
         // Dibuix
@@ -114,9 +193,17 @@ int main()
             window.draw(clouds[i].sprite);
         }
 
+        for (int i = 0; i < NUM_BRANCHES; i++) {
+            window.draw(branches[i]);
+        }
+
         window.draw(spriteTree1);
         window.draw(spritePlayer);
         window.draw(bee.sprite);
+        window.draw(scoreText);
+        window.draw(timeBar);
+
+        if (paused) window.draw(messageText);
 
         window.display();
     }
@@ -127,7 +214,7 @@ int main()
 void updateNPC(NPC& npc, float dt) {
     if (!npc.active) {
         npc.speed = (rand() % npc.maxSpeed) * npc.sentit;
-        float height = rand() % npc.maxHeight;
+        float height = static_cast<float>(rand() % npc.maxHeight);
         npc.sprite.setPosition({ npc.posicioInicialX, height });
         npc.active = true;
     }
@@ -137,6 +224,42 @@ void updateNPC(NPC& npc, float dt) {
             npc.sprite.getPosition().y
         });
         
-        if (npc.sprite.getPosition().x < -200 || npc.sprite.getPosition().x > 200) npc.active = false;
+        if (npc.sprite.getPosition().x < -200 || npc.sprite.getPosition().x > 2000) npc.active = false;
+    }
+}
+
+void updateBranchSprites(Side branchPositions[], Sprite branches[]) {
+    for (int i = 0; i < NUM_BRANCHES; i++) {
+        float height = i * 150;
+
+        if (branchPositions[i] == Side::LEFT) {
+            branches[i].setPosition({ 610, height });
+            branches[i].setRotation(degrees(180));
+        }
+        else if (branchPositions[i] == Side::RIGHT) {
+            branches[i].setPosition({ 1330, height });
+            branches[i].setRotation(degrees(0));
+        }
+        else {
+            branches[i].setPosition({ 3000, height });
+        }
+    }
+}
+
+void updateBranches(Side branchPositions[], int seed) {
+    for (int j = NUM_BRANCHES - 1; j > 0; j--)
+        branchPositions[j] = branchPositions[j - 1];
+    srand((int)time(0) + seed);
+    int r = rand() % 5;
+    switch (r) {
+    case 0:
+        branchPositions[0] = Side::LEFT;
+        break;
+    case 1:
+        branchPositions[0] = Side::RIGHT;
+        break;
+    default:
+        branchPositions[0] = Side::NONE;
+        break;
     }
 }
